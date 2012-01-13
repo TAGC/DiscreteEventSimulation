2903ca257fde2a9c58cd61aa57a13ef2f9cacb07
Dr Tristan Allwood <tora@doc.ic.ac.uk>
2012-01-13 18:56:08 +0000

    Initial import.

---
+++ src/picture/Main.java
@@ -0,0 +1,11 @@
+package picture;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: Implement this.
+        System.err.println("TODO: Implement main");
+        System.exit(-1);
+    }
+}
